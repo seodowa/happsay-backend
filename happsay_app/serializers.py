e48ae204c49010a6edc97e1244dc86f734908591
seodowa <pagalan.theodoreustp@gmail.com>
2025-02-11 23:29:43 +0800

    added api functionality

---
+++ happsay_app/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from .models import TodoList
+from rest_framework import serializers
+
+
+class TodoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoList
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
--- happsay_app/views.py
+++ happsay_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from .serializers import UserSerializer, TodoListSerializer
+from django.contrib.auth.models import User
+from .models import TodoList
+
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TodoListViewSet(viewsets.ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
