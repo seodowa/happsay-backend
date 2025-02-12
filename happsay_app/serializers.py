b4c0442e93cc40fbef5958b988321c260a415098
seodowa <pagalan.theodoreustp@gmail.com>
2025-02-12 23:11:21 +0800

    modified settings.py

--- happsay_app/serializers.py
+++ happsay_app/serializers.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import User
 from .models import TodoList
-from rest_framework import serializers # type: ignore
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
 
 
 class TodoListSerializer(serializers.ModelSerializer):
@@ -10,6 +12,24 @@ class TodoListSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
+
+
+    def create(self, validated_data):
+
+        validated_data['password'] = make_password(validated_data['password'])  # Hash password
+
+        return super().create(validated_data)
+
+
+    def update(self, instance, validated_data):
+
+        if 'password' in validated_data:
+
+            validated_data['password'] = make_password(validated_data['password'])  # Hash on update
+
+        return super().update(instance, validated_data)
--- happsay_backend/settings.py
+++ happsay_backend/settings.py
@@ -38,13 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
+    "rest_framework",
     "happsay_app",
-    'corsheaders'
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddlew are"
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
