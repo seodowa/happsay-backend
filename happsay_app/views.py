5e7f4883395986cd41f15194949c431fa2177d84
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-02 23:30:54 +0800

    m

--- happsay_app/views.py
+++ happsay_app/views.py
@@ -131,7 +131,9 @@ class PasswordResetView(APIView):
 
         token = RefreshToken.for_user(user)
 
-        reset_link = request.build_absolute_uri(reverse('password_reset_confirm', kwargs={'token': str(token)}))
+        # temporary solution to get the reset link
+        #reset_link = request.build_absolute_uri(reverse('password_reset_confirm', kwargs={'token': str(token)}))
+        reset_link = f"http://localhost:5173/ResetPass/password_reset_confirm/{str(token)}"
 
         send_mail(
             'Password Reset Request',
