83e692817c50b5d1ca8ec9d99f0f2c81d4f0b658
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-03 23:27:14 +0800

    m

--- happsay_backend/settings.py
+++ happsay_backend/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]    # Set to your Django secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "happsay-backend-dev.ap-southeast-1.elasticbeanstalk.com", "172.31.5.224", "172.31.16.223"]
+ALLOWED_HOSTS = ["127.0.0.1", 
+                 "happsay-backend-dev.ap-southeast-1.elasticbeanstalk.com", 
+                 "172.31.5.224", 
+                 "172.31.16.223",
+                 "52.74.209.129",
+                 "122.248.207.37",
+                 ]
 
 
 # Application definition
