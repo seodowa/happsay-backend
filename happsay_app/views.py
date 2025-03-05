00ff5ea1a0a07f91c637a3c3ae610b76519fd201
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-05 15:18:22 +0800

    m

--- happsay_app/views.py
+++ happsay_app/views.py
@@ -84,7 +84,7 @@ class RegisterAPIView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
