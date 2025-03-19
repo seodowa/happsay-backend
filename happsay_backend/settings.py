ea7398bd49c4a963f1643a7ca8bdcd76eb89b3d9
seodowa <pagalan.theodoreustp@gmail.com>
2025-03-19 22:50:55 +0800

    conditional load_dotenv

--- happsay_backend/settings.py
+++ happsay_backend/settings.py
@@ -16,12 +16,16 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-if load_dotenv():
-    env = environ.Env()
-    environ.Env.read_env()
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.getenv("VERCEL") is None:
+    load_dotenv()
+    
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
